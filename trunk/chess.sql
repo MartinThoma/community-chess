af8c59be1caad1f64cd0501a31e7018e62d09e7a
themoosemind@gmail.com <themoosemind@gmail.com@43eade66-a1a0-1168-f01e-b03ee1686788>
2011-07-28 13:20:49 +0000

    accidentially deleted some tables from chess.sql. Re-added them now

--- trunk/chess.sql
+++ trunk/chess.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 28, 2011 at 02:50 PM
+-- Generation Time: Jul 28, 2011 at 03:19 PM
 -- Server version: 5.1.49
 -- PHP Version: 5.3.3-1ubuntu9.5
 
@@ -39,6 +39,168 @@ CREATE TABLE IF NOT EXISTS `chess_currentGames` (
 
 --
 -- Dumping data for table `chess_currentGames`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_medalPlayerCorrelation`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_medalPlayerCorrelation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `playerID` int(11) NOT NULL,
+  `medalID` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `chess_medalPlayerCorrelation`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_medals`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_medals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL COMMENT 'URL',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `chess_medals`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_pastGames`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_pastGames` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `moveList` text NOT NULL,
+  `whitePlayerID` int(11) NOT NULL,
+  `blackPlayerID` int(11) NOT NULL,
+  `whitePlayerSoftwareID` int(11) NOT NULL,
+  `blackPlayerSoftwareID` int(11) NOT NULL,
+  `outcome` tinyint(4) NOT NULL COMMENT '0 means white won, 1 means black won, 2 means draw',
+  `startTime` datetime NOT NULL,
+  `endTime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `chess_pastGames`
+--
+
+INSERT INTO `chess_pastGames` (`id`, `moveList`, `whitePlayerID`, `blackPlayerID`, `whitePlayerSoftwareID`, `blackPlayerSoftwareID`, `outcome`, `startTime`, `endTime`) VALUES
+(1, 'dsfadf', 2, 1, 0, 0, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_players`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uname` varchar(255) NOT NULL,
+  `upass` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `chess_players`
+--
+
+INSERT INTO `chess_players` (`id`, `uname`, `upass`) VALUES
+(1, 'abc', '900150983cd24fb0d6963f7d28e17f72'),
+(2, 'test', '098f6bcd4621d373cade4e832627b4f6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_software`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_software` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `version` varchar(20) NOT NULL,
+  `lastVersionID` int(11) NOT NULL COMMENT '0 if this is the first version',
+  `changelog` text NOT NULL,
+  `BT2450Rating` int(11) NOT NULL,
+  `EloRating` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `chess_software`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_softwareDeveloper`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_softwareDeveloper` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `playerID` int(11) NOT NULL,
+  `softwareID` int(11) NOT NULL,
+  `task` varchar(255) NOT NULL COMMENT 'What did this person do? What was his/her job?',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `chess_softwareDeveloper`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_turnamentGames`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_turnamentGames` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `turnamentID` int(11) NOT NULL,
+  `gameID` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL COMMENT 'current or past game?',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `chess_turnamentGames`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chess_turnaments`
+--
+
+CREATE TABLE IF NOT EXISTS `chess_turnaments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `chess_turnaments`
 --
 
 
