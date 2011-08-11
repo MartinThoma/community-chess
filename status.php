0df74d6aed2de492c41e57171a66bea1265330a4
themoosemind@gmail.com <themoosemind@gmail.com@43eade66-a1a0-1168-f01e-b03ee1686788>
2011-08-11 10:55:07 +0000

    added my_software.php, status.ph and tournaments.php

---
+++ status.php
@@ -0,0 +1,24 @@
+<?
+/**
+ * @author: Martin Thoma
+ * running games, who is white/black
+ * */
+
+require ('wrapper.inc.php');
+if(USER_ID === false){exit("Please <a href='login.wrapper.php'>login</a>");}
+
+$condition = "WHERE `whitePlayerID`=".USER_ID." OR `blackPlayerID`=".USER_ID;
+
+$row = selectFromTable(array('id'), 'chess_currentGames', $condition);
+echo 'Current game-IDs:<br/>';
+foreach($row as $gameId){
+    echo $gameId."<br/>";
+}
+
+$row = selectFromTable(array('id'), 'chess_pastGames', $condition);
+echo 'Past game-IDs:<br/>';
+foreach($row as $gameId){
+    echo $gameId."<br/>";
+}
+
+?>
