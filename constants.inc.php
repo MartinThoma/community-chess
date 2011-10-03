367f76e6849c45fd571131a5753f552e2361834f
themoosemind@gmail.com <themoosemind@gmail.com@43eade66-a1a0-1168-f01e-b03ee1686788>
2011-10-03 18:46:27 +0000

    corrected typing errors

--- constants.inc.php
+++ constants.inc.php
@@ -6,8 +6,8 @@
 $table_prefix = 'chess_';
 define('USERS_TABLE',                      $table_prefix.'users');
 define('GAMES_TABLE',                      $table_prefix.'games');
-define('TURNAMENTS_TABLE',                 $table_prefix.'turnaments');
-define('TURNAMENT_PLAYERS_TABLE',          $table_prefix.'turnamentPlayers');
+define('TOURNAMENTS_TABLE',                $table_prefix.'tournaments');
+define('TOURNAMENT_PLAYERS_TABLE',         $table_prefix.'tournamentPlayers');
 define('SOFTWARE_TABLE',                   $table_prefix.'software');
 define('SOFTWARE_USER_TABLE',              $table_prefix.'softwareUsers');
 define('SOFTWARE_DEVELOPER_TABLE',         $table_prefix.'softwareDeveloper');
