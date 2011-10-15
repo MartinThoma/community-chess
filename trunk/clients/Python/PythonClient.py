33c0905d8cef124ca7dc5b00db52672509f5c1d4
themoosemind@gmail.com <themoosemind@gmail.com@43eade66-a1a0-1168-f01e-b03ee1686788>
2011-10-15 21:06:20 +0000

    Python Client works now.

--- trunk/clients/Python/PythonClient.py
+++ trunk/clients/Python/PythonClient.py
@@ -26,6 +26,17 @@ class ChessClient(object):
         self.myTurn = (self.myColor == self.colorsTurn)
         self.gameLoop()
 
+    def __repr__(self):
+        pythonRepresentation  = ("Community Chess Client class\n")
+        pythonRepresentation += ("Board:\n")
+        for y in xrange(0,8):
+            row = []
+            for x in xrange(0, 8):
+                row.append(str(self.board[x][y]))
+            pythonRepresentation += "%i. %s\n" % (y+1, str(row)) #("%s\n" % str(row))
+
+        return pythonRepresentation
+
     def login(self):
         """ Login with the given credentials. Save the cookie. """
         url = self.baseUrl + '?action=login&username=' + self.username + \
@@ -33,11 +44,15 @@ class ChessClient(object):
         u = urllib.URLopener();x = u.open(url)
         return x.read()
 
-    def getCurrentGames(self):
-        """ Get a list with all current Game-IDs. """
-        url = self.baseUrl + '?action=listCurrentGames'
+    def sendRequestWithCookie(self, getstring):
+        """ This method is only for usage within the Client Class """
+        url = self.baseUrl + getstring
         u = urllib.URLopener();u.addheader('Cookie',self.cookie);x = u.open(url)
         content = x.read()
+        return content
+
+    def parseList(self, content):
+        """ This helper method parses a string to a list. """
         if '::' in content:
             liste = content.split('::')
         else:
@@ -45,27 +60,31 @@ class ChessClient(object):
                 liste = [content]
             else:
                 liste = []
+        return liste
+
+
+    def getCurrentGames(self):
+        """ Get a list with all current Game-IDs. """
+        content = self.sendRequestWithCookie('?action=listCurrentGames')
+        liste   = self.parseList(content)
         return liste
 
     def getPlayerIDs(self):
         """ Get a list with all players you can challenge. """
-        url = self.baseUrl + '?action=getPlayerIDs'
-        u = urllib.URLopener();u.addheader('Cookie',self.cookie);x = u.open(url)
-        content = x.read()
-        if '::' in content:
-            liste = content.split('::')
-        else:
-            if int(content) != None:
-                liste = [content]
-            else:
-                liste = []
+        content = self.sendRequestWithCookie('?action=getPlayerIDs')
+        liste   = self.parseList(content)
         return liste
 
     def challengePlayer(self, playerID):
         """ Challenge the player with the ID playerID. """
-        url = self.baseUrl + '?action=challengeUser&user_id=' + playerID
-        u = urllib.URLopener();u.addheader('Cookie',self.cookie);x = u.open(url)
-        return x.read()
+        content = self.sendRequestWithCookie('?action=challengeUser&user_id=' + playerID)
+        return content
+
+    def submitMove(self, move):
+        """ Challenge the player with the ID playerID. """
+        query = "?gameID=%s&move=%s" % (self.currentGameID, move)
+        content = self.sendRequestWithCookie(query)
+        return content
 
     def getBoard(self):
         url = self.baseUrl + '?gameID=' + self.currentGameID + '&action=getBoard'
@@ -76,9 +95,9 @@ class ChessClient(object):
             for x in xrange(0, 8):
                 pos = x + y*8
                 if content[pos:(pos+1)] in ['p', 'P']:
-                    board[x][y] = Pawn(self, content[pos:(pos+1)], x, y)
+                    board[x][y] = Pawn(content[pos:(pos+1)], x, y)
                 else:
-                    board[x][y] = content[pos:(pos+1)]
+                    board[x][y] = ChessPiece(content[pos:(pos+1)], 0, x, y)
         self.board = board
         return board
 
@@ -98,7 +117,52 @@ class ChessClient(object):
         if self.board[x][y] == '0':
             return False
         else:
-            return self.board[x][y].isupper()
+            if self.myColor == 'white':
+                return self.board[x][y].letter.islower()
+            else:
+                return self.board[x][y].letter.isupper()
+
+    def isOwn(self, x, y):
+        if self.board[x][y] == '0':
+            return False
+        else:
+            if self.myColor == 'white':
+                return self.board[x][y].letter.isupper()
+            else:
+                return self.board[x][y].letter.islower()
+
+    def isEmpty(self, x, y):
+        return self.board[x][y].letter == '0'
+
+    def getPossibleMoves(self, piece):
+        """ Get a list of vectors for all possible moves. """
+        moveVectors = []
+
+        standard = piece.standardMoves()
+        for move in standard:
+            if (piece.x + move[0]) < 8 and (piece.y + move[1]) < 8 \
+                and self.isEmpty(move[0] + piece.x, move[1] + piece.y):
+                moveVectors.append(move)
+
+        capture = piece.captureMoves()
+        for move in capture:
+            if (piece.x + move[0]) < 8 and (piece.y + move[1]) < 8 and \
+                self.isEnemy(move[0], move[1]):
+                moveVectors.append(move)
+
+        return moveVectors
+
+    def makeMove(self):
+        for y, row in enumerate(self.board):
+            for x, element in enumerate(row):
+                if self.isOwn(x, y):
+                    for vector in self.getPossibleMoves(self.board[x][y]):
+                        move = "%i%i%i%i" % (x+1, y+1, x+vector[0]+1, y+vector[1]+1)
+                        content = self.submitMove(move)
+                        if not ('ERROR' in content):
+                            return True
+        return False
+                        
 
     def gameLoop(self):
         while self.myTurn == False:
@@ -106,6 +170,7 @@ class ChessClient(object):
             time.sleep(1)
             self.colorsTurn = self.whoseTurnIsIt()
             self.myTurn = (self.myColor == self.colorsTurn)
-        print self.getBoard()
+        self.getBoard()
+        self.makeMove()
 
 
--- trunk/clients/Python/main.py
+++ trunk/clients/Python/main.py
@@ -6,4 +6,4 @@ from PythonClient import *
 client = ChessClient('test', 'test')
 
 # Which moves are possible for the piece at (0|3)?
-print client.board[0][3].getPossibleMoves(client)
+print client.getPossibleMoves(client.board[0][6])
