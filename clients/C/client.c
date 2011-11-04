cf892abb8fce419f1fac250f6ec3faa4d9fa7398
themoosemind@gmail.com <themoosemind@gmail.com@43eade66-a1a0-1168-f01e-b03ee1686788>
2011-11-04 20:30:30 +0000

    Improvements for C client and Java client

--- clients/C/client.c
+++ clients/C/client.c
@@ -1,41 +1,41 @@
-#define CURL_STATICLIB
+/**
+ * This C client should provide some very basic actions for communication with the
+ * Communit Chess server
+ *
+ * I've used snippets from the following web pages:
+ *   http://rosettacode.org/wiki/Web_scraping#C
+ *
+ * @category Web_Services
+ * @package  Community-chess
+ * @author   Martin Thoma <info@martin-thoma.de>
+ * @license  http://www.opensource.org/licenses/mit-license.php  MIT License
+ * @version  SVN: <svn_id>
+ * @link     http://code.google.com/p/community-chess/
+ */
+
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /* Install curl (sudo apt-get install curl libcurl3-dev) if this throws an error */
 #include <curl/curl.h>
-#include <curl/types.h>
-#include <curl/easy.h>
-#include <string.h>
-
-size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    size_t written;
-    written = fwrite(ptr, size, nmemb, stream);
-    return written;
+ 
+#define BUFSIZE 16384
+ 
+size_t lr = 0;
+ 
+size_t filterit(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+    if ( (lr + size*nmemb) > BUFSIZE ) return BUFSIZE;
+    memcpy(stream+lr, ptr, size*nmemb);
+    lr += size*nmemb;
+    return size*nmemb;
 }
-
-char* read_data() {
-    // thanks to Johannes Schaub - litb
-    // http://stackoverflow.com/questions/410943/reading-a-text-file-into-an-array-in-c
-    FILE *f = fopen("returnedval.txt", "rb");
-    fseek(f, 0, SEEK_END);
-    long pos = ftell(f);
-    fseek(f, 0, SEEK_SET);
-
-    char *bytes = malloc(pos);
-    fread(bytes, pos, 1, f);
-    fclose(f);
-
-    return(bytes); // do some stuff with it
-    free(bytes); // free allocated memory - where do I need this?
-}
-
 
 char * queryWebpage(char *url_query) {
     /* This function sends a request to the xhrframework of community-chess.com */
+    CURL *curlHandle;
+    char buffer[BUFSIZE];
 
-    CURL *curl;
-    FILE *fp;
-    CURLcode res;
     char *base_url = "http://community-chess.com/xhrframework.php";
     char *url;
 
@@ -44,31 +44,30 @@ char * queryWebpage(char *url_query) {
     strcpy(url, base_url); /* copy name into the new var */
     strcat(url, url_query); /* add the extension */
 
-    char outfilename[FILENAME_MAX] = "returnedval.txt";
-    curl = curl_easy_init();
-    if (curl) {
-        fp = fopen(outfilename,"wb");
-        curl_easy_setopt(curl, CURLOPT_URL, url);
+    curlHandle = curl_easy_init();
+    curl_easy_setopt(curlHandle, CURLOPT_URL, url);
+    curl_easy_setopt(curlHandle, CURLOPT_FOLLOWLOCATION, 1);
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, filterit);
+    curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, buffer);
+    int success = curl_easy_perform(curlHandle);
+    curl_easy_cleanup(curlHandle);
 
-        // Add header if the cookie has been set
-        curl_easy_setopt(curl, CURLOPT_HEADER, 0);
-
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 180);
-
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-        fclose(fp);
+    if (success != 0) {
+        printf("No success from curl_easy_perform.\n");
     }
 
-    return read_data();
+    buffer[lr] = 0;
+    char *returnval = malloc(sizeof(char)*10);
+    sprintf(returnval, "%s", &buffer[0]);
+    return returnval;
 }
 
-int main(void) {
-	char * returnedval = queryWebpage("?action=login&username=abc&password=abc");
-    printf("%s\n", returnedval);
+int main()
+{
+    //char phpsessid[33];
+    //phpsessid = queryWebpage("?action=login&username=abc&password=abc");
+	char * phpsessid = queryWebpage("?action=login&username=abc&password=abc");
+    printf("%s\n", phpsessid);
 
     return 0;
 }
---
+++ clients/Java/Pawn.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Pawn extends ChessPiece {
+    int normalMoves[][] = {{0,1}, {0,-1}};
+    String name = "Pawn";
+
+    public Pawn () {
+        this.name = "Pawn";
+        this.normalMove = normalMove;
+    }
+
+    //ArrayList<Integer> normalMove = new ArrayList<Integer>();
+    //normalMove.add("1");
+
+    //ArrayList arrayList = new ArrayList();
+    //arrayList<Vector>.add(v);
+
+    //normalMove  = arrayList;
+    //ArrayList<Vector> captureMove = new ArrayList<Vector>();
+    //String color;
+}
