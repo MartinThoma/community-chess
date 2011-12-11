96dcab1f477e3fe5fed05eb3a22338fcabd6c233
themoosemind@gmail.com <themoosemind@gmail.com@43eade66-a1a0-1168-f01e-b03ee1686788>
2011-12-11 13:12:58 +0000

    Java Client was updated

--- trunk/clients/Java/ChessPiece.java
+++ trunk/clients/Java/ChessPiece.java
@@ -4,21 +4,26 @@ import java.util.ArrayList;
  *  The ChessPiece class is the superclass for all chess pieces and
  *  the empty field.
  */
-public class ChessPiece {
+public abstract class ChessPiece {
     /** The position is represented as two Integers. */
-    private int[] position;
+    protected int[] position = new int[2];
     /** The color is either 'black' or 'white'. */
     private String color;
     /** Normal moves. Each move is an array of two Integers. */
-    protected ArrayList < int[] > normalMove  = new ArrayList < int[] > ();
+    protected ArrayList < int[] > normalMove  = new ArrayList < int[] >();
     /** capture moves. Like normal moves. */
-    protected ArrayList < int[] > captureMove = new ArrayList < int[] > ();
+    protected ArrayList < int[] > captureMove = new ArrayList < int[] >();
     /** The name is either 'Pawn' or 'Empty'. */
     protected String name;
 
-    /** Constructor. */
-    public ChessPiece() {
+    /** Constructor.
+     * @param x the x-coordinate of the new piece on the chess board
+     * @param y the y-coordinate of the new piece on the chess board
+     **/
+    public ChessPiece(final int x, final int y) {
         this.name = "Empty";
+        this.position[0] = x;
+        this.position[1] = y;
     }
 
     /** Overriding toString.
@@ -28,23 +33,27 @@ public class ChessPiece {
         return "ChessPiece - " + color + " - " + name;
     }
 
-    /** Getter and setter.
+    /** Get the position of the current chess piece.
+     * @return the positon as an int array.
+     */
+    public final int[] getPosition() {
+        return position;
+    }
+
+    /** Get all capture moves of a chess piece.
+     * @return the capture moves
+     */
+    public final ArrayList < int[] > getCaptureMoves() {
+        return captureMove;
+    }
+
+    /** Get the Name of the Chess Piece, e.g. Pawn or King.
       * @return The name of the Object.
       */
-    public String getName() {
+    public final String getName() {
         return this.name;
     }
 
     /** Move the chess piece. */
-    public void move() {
-        int x = this.position[0];
-        int y = this.position[1];
-        int toX, toY;
-        for (int[] move : normalMove) {
-            ChessClient.getWebSite("?gameID=1&move="
-                + x + y
-                + (x + move[0])
-                + (y + move[1]));
-        }
-    }
+    public abstract void move();
 }
--- trunk/clients/Java/Pawn.java
+++ trunk/clients/Java/Pawn.java
@@ -1,11 +1,34 @@
 /** The Pawn class represents the pawn chess piece. */
 
 public class Pawn extends ChessPiece {
-    /** Constructor. */
-    public Pawn() {
+    /** An array of the standard moves of a ChessPiece. */
     int[][] normalMoves = {{0, 1}, {0, -1}};
+
+    /** Constructor.
+     * @param x the x-coordinate of the new piece on the chess board
+     * @param y the y-coordinate of the new piece on the chess board
+     **/
+    public Pawn(final int x, final int y) {
+        super(x, y);
         this.name = "Pawn";
-        this.normalMove = normalMove;
+        this.position[0] = x;
+        this.position[1] = y;
+    }
+
+    /** Move the chess piece. */
+    public final void move() {
+        int x = this.position[0];
+        int y = this.position[1];
+        int toX;
+        int toY;
+        for (int[] move : normalMove) {
+            toX =  x + move[0];
+            toY =  y + move[1];
+            ChessClient.getWebSite("?gameID=1&move="
+                + x + y
+                + toX
+                + toY);
+        }
     }
 
     //ArrayList<Integer> normalMove = new ArrayList<Integer>();
